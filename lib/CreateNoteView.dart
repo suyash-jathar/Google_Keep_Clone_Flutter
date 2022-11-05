7afbd4371723d9e1b5e34c1484e89ac5e52d3384
suyash-jathar <suyashjathar15@gmail.com>
2022-11-05 16:11:08 +0530

    Added dispose override for enhancement

--- lib/CreateNoteView.dart
+++ lib/CreateNoteView.dart
@@ -14,6 +14,13 @@ class _CreateNoteViewState extends State<CreateNoteView> {
   TextEditingController title=TextEditingController();
   TextEditingController content=TextEditingController();
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    title.dispose();
+    content.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgcolor,
